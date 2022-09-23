d212d2c1d6cbb37e00ec9aa54240e8fc5c2dc1f4
Lutsko Michael <lutsko86@gmail.com>
2022-09-23 16:30:04 +0300

    Create Rooms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :rememberable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :validatable
+  has_many :rooms, dependent: :destroy
+
+  devise :database_authenticatable, :registerable, :recoverable, :validatable
 end
