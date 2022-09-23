d696427571f14cd0ded3a2bf6cb7fcee5b28d08f
Lutsko Michael <lutsko86@gmail.com>
2022-09-23 16:55:45 +0300

    add message likes counter_cache

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :message
+  belongs_to :message, counter_cache: true
 
   validates :user_id, uniqueness: { scope: :message_id }
 end
